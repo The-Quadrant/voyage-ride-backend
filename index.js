36bfa3bf65a0c879f0bc922bfe9abda175b4aca2
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-06-18 01:38:47 +0100

    initial Chnages made to Branch

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require("express");
+const express = require('express')
 const dotenv = require("dotenv");
+const mongoose = require('mongoose');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+// Connect to mongodb in the .env files.
+
+	// t1 and t2 is to calc Mongo Connect time
+const t1 = new Date();
+
+const main = async () => {
+  await mongoose.connect(process.env.MONGO_URL, {
+    
+  })
+
+  .then(console.log('Mongo'))
+
+
+}
+main() .catch(err => console.log(err));
+const t2 = new Date();
+console.log(`${t2-t1} MSecs`);
+
+
 
 const app = express();
 
-app.listen(PORT, () => {
-	console.log(`server running on port ${PORT}`);
-});
+
+app.listen( "5000", () => {
+	console.log('Node runing on 5000')
+})
